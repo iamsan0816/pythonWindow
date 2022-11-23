6c731d4e90dd2644c49212eeca6767e6c9c79888
iamsan0816 <iamsan0816@gmail.com>
2022-11-23 13:59:54 +0800

    datasource as ds

--- 1119Python/專案3/datasource.py
+++ 1119Python/專案3/datasource.py
@@ -1,13 +1,32 @@
 # 自訂的module (模組) 下載資料和處理資料
 # pipy 成千上萬的套件可以使用  reuests
 import requests
-
+tw_county_names = {"台北": "Taipei",
+                   "台中": "Taichung",
+                   "基隆": "Keelung",
+                   "台南": "Tainan",
+                   "高雄": "Kaohsiung",
+                   "新北": "New Taipei",
+                   "宜蘭": "Yilan",
+                   "桃園": "Taoyuan",
+                   "嘉義": "Chiayi",
+                   "新竹": "Hsinchu",
+                   "苗栗": "Miaoli",
+                   "南投": "Nantou",
+                   "彰化": "Changhua",
+                   "雲林": "Yunlin",
+                   "屏東": "Pingtung",
+                   "花蓮": "Hualien",
+                   "台東": "Taitung",
+                   "金門": "Kinmen",
+                   "澎湖": "Penghu",
+                   "連江": "Lienchiang"
+                   }
 
 api_key = "6c88d8b593da55e5d8bf61855950dee6"
 cityName = "Taipei"
 
-
-def get_forcast_data():
+def get_forcast_data(cityName):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
 
 
@@ -16,7 +35,7 @@ def get_forcast_data():
     if response.ok:
         print("下載成功")
         # print(response.text)
-        return response.json()
+        return response.json()['list']
     else:
         print("下載失敗")
 
--- 1119Python/專案3/main.py
+++ 1119Python/專案3/main.py
@@ -1,11 +1,13 @@
-import datasource
+import datasource as ds
 
 
 def main():
     # print("這裏是main function")
-    all_data = datasource.get_forcast_data()
-    print(all_data)
-
+    # all_data = datasource.get_forcast_data()
+    list_data = ds.get_forcast_data(ds.tw_county_names["基隆"])
+    # print(type(all_data))
+    for item in list_data:
+        print(item['dt_txt'])
 
 if __name__ == "__main__":
     print("這裏是程式的執行點")
